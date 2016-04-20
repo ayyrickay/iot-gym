03d30a012b7f98457f6026b6f062509514ed8496
Ricky <ricky.holtz@berkeley.edu>
2016-04-20 13:42:44 -0700

    Set up tests and full paths for server

--- assets/js/activityStream.js
+++ assets/js/activityStream.js
@@ -1,31 +1,30 @@
 "use strict";
 
 var exports = module.exports = {
-  publishASBaseData: function publishASBaseData() {
-
+  publishActivityStream: function publishActivityStream(asData){
     // Create ActivityStream (corresponds to the example in the ASBase API Doc)
     var asData = {};
   }
 }
 
-var asBaseURL = 'http://russet.ischool.berkeley.edu:8080';
+
 
 //JOB: Builds ASBase Data for activity streams
-exports.ASBuilder = function(actor, verb, object){
-  var asData = {
-    "actor": actor,
-    "verb" : verb,
-    "object": object
+exports.checkJSON = function(object){
+  try{
+    JSON.parse(object);
+  } catch (e){
+    //invalid JSON input, return null
+    return null;
   }
-
-  return asData
 };
 
 //JOB: publishes an event to ASBase
 function publishEvent(asData, asBaseURL) {
 
-  // Load restler library for HTTP requests
+  // Load restler library for HTTP requests, and store ASBase URL
   var rest = require('restler');
+  var asBaseURL = 'http://russet.ischool.berkeley.edu:8080';
 
   // Log message
   console.log('Publishing: ' + JSON.stringify(asData));
@@ -41,7 +40,7 @@ function publishEvent(asData, asBaseURL) {
       'Content-Type': 'application/stream+json'
     }
 
-  }).on('compvare', function(data, response) {
+  }).on('compare', function(data, response) {
 
     // Check that the correct response code was received
     if (response.statusCode === 200) {
